2600dd68471a92572df4d317cabbcb600d40484f
Maverianne <64marianamv@gmail.com>
2024-05-27 12:49:33 -0600

    instruction fix

--- Assets/Scripts/UI/Instructions.cs
+++ Assets/Scripts/UI/Instructions.cs
@@ -7,20 +7,37 @@ namespace UI
     public class Instructions : MonoBehaviour
     {
         [SerializeField] private CanvasGroup[] slides;
+        [SerializeField] private CanvasGroup intructionsCanvasGroup;
+
+
+        private bool _changingSlides;
+        private bool TerminateSlide => _currentSlide == slides.Length;
+        private int _currentSlide;
 
         public void SetUpInstructions()
         {
-            foreach (var VARIABLE in SetUpInstructions()) ;
+            foreach (var slide in slides)
             {
-                
-            }            
+                slide.alpha = 0;
+            }
+
+            
+            slides[_currentSlide].alpha = 1;
         }
 
         public void NextSlide()
         {
+            if (TerminateSlide)
+            {
+                
+            }
+            slides[_currentSlide].alpha = 0;
+        }
+
+        private void TerminateInstructions()
+        {
             
         }
-        
         private IEnumerator PerformFade(float duration, bool fadeIn, CanvasGroup canvasGroup, bool fadeBackground = true)
         {
             var timer = 0f;
